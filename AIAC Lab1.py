75fd2c13d7b20c33e7257204d025c89ccb4938b2
Mohammed-Fawaz-ali <mdfawazali18@gmail.com>
2026-01-20 13:45:43 +0530

    Lab4 Assignment

--- AIAC Lab1.py
+++ AIAC Lab1.py
@@ -1,4 +1,4 @@
-#Generate code for the fibonacci sequence up to n terms without functions
+"""#Generate code for the fibonacci sequence up to n terms without functions
 n = int(input("Enter the number of terms in the Fibonacci sequence: "))
 a, b = 0, 1
 count = 0
@@ -12,10 +12,10 @@ else:
     while count < n:
         print(a, end=' ')
         a, b = b, a + b
-        count += 1
+        count += 1"""
 
 
-# Simplify the code by removing unnecessary variables and conditions. optimze the code 
+"""# Simplify the code by removing unnecessary variables and conditions. optimze the code 
 n = int(input("Enter the number of terms in the Fibonacci sequence: "))
 a, b = 0, 1
 if n <= 0:
@@ -24,9 +24,9 @@ else:
     print("Fibonacci sequence up to", n, "terms:")
     for _ in range(n):
         print(a, end=' ')
-        a, b = b, a + b
+        a, b = b, a + b"""
 
-#Generate code for the fibonacci sequence up to n terms with functions
+"""#Generate code for the fibonacci sequence up to n terms with functions
 def fibonacci(n):
     a, b = 0, 1
     sequence = []
@@ -39,10 +39,10 @@ if n <= 0:
     print("Please enter a positive integer.")
 else:
     print("Fibonacci sequence up to", n, "terms:")
-    print(' '.join(map(str, fibonacci(n))))
+    print(' '.join(map(str, fibonacci(n))))"""
 
-#Give me short report on fibonacci sequence code with and without function
-"""The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones,
+"""#Give me short report on fibonacci sequence code with and without function
+The Fibonacci sequence is a series of numbers where each number is the sum of the two preceding ones,
 usually starting with 0 and 1. The code provided generates the Fibonacci sequence up to 'n' terms in two different ways:
 without functions and with functions."""
 
---
+++ Lab4.py
@@ -0,0 +1,72 @@
+#write python code for prime number without function
+num = int(input("Enter a number: "))
+if num > 1:
+    for i in range(2, int(num/2)+1):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
+
+#Write python code for sum of all elements in a list using function Eg:- Input: [1,2,3,4] Output: 10 
+def sum_list(lst):
+    total = 0
+    for i in lst:
+        total += i
+    return total
+input_list = [1, 2, 3, 4]
+result = sum_list(input_list)
+print("The sum of all elements in the list is:", result)
+
+#Write a python program to extract digits from alphanumeric string 
+# Eg:- Input: "abc123xyz" Output: 123 
+# Input: "a1b2c3" Output: 123 
+# Input: "no digits here" Output: 0
+import re
+def extract_digits(s):
+    digits = re.findall(r'\d', s)
+    if digits:
+        return int(''.join(digits))
+    else:
+        return 0
+input_str1 = "abc123xyz"
+input_str2 = "a1b2c3"
+input_str3 = "no digits here"
+print("Extracted digits from '{}': {}".format(input_str1, extract_digits(input_str1)))
+print("Extracted digits from '{}': {}".format(input_str2, extract_digits(input_str2)))
+print("Extracted digits from '{}': {}".format(input_str3, extract_digits(input_str3)))
+
+# write python program to count number of vowels in a string.
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in s:
+        if char in vowels:
+            count += 1
+    return count
+input_string = "Hello World"
+vowel_count = count_vowels(input_string)
+print("Number of vowels in '{}': {}".format(input_string, vowel_count))
+
+# Again Write a python program to count number of volwels in a string. 
+# Eg:- Input: "Hello World" Output: 3 
+# Input: "Python Programming" Output: 4
+
+def count_vowels(s):
+    vowels = "aeiouAEIOU"
+    count = 0
+    for char in s:
+        if char in vowels:
+            count += 1
+    return count
+input_string1 = "Hello World"
+input_string2 = "Python Programming"
+vowel_count1 = count_vowels(input_string1)
+vowel_count2 = count_vowels(input_string2)
+print("Number of vowels in '{}': {}".format(input_string1, vowel_count1))
+print("Number of vowels in '{}': {}".format(input_string2, vowel_count2))
+
+#Comaprision between zero-shot and few-shot prompting
+#Zero-shot prompting involves providing the model with a task or question without any prior examples or context
+#Few-shot prompting, on the other hand, involves providing the model with a few examples or context to help it understand the task better before asking it to generate a response.
+
